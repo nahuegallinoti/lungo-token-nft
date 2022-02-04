d8d17f727285d105d0bcce699064d52b492dfd17
Dan <danii-benitez@hotmail.com>
2022-02-04 17:28:35 -0300

    [fix] navbar

--- market-react/src/components/shared/NavbarComponent.jsx
+++ market-react/src/components/shared/NavbarComponent.jsx
@@ -40,7 +40,7 @@ export const NavbarComponent = () => {
     return (
 
       <header className="text-gray-400 bg-gray-900 body-font">
-        <div className="container flex flex-col flex-wrap items-center justify-between p-5 mx-auto md:flex-row">
+        <div className="flex flex-col flex-wrap items-center justify-between p-5 mx-auto md:flex-row">
           <NavLink to='/' className="flex items-center mb-4 font-medium text-white title-font md:mb-0">
             <svg xmlns="http://www.w3.org/2000/svg"  fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-10 h-10 p-2 text-white bg-purple-500 rounded-full">
               <path d="M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z"></path><polyline points="3.27 6.96 12 12.01 20.73 6.96"></polyline><line x1="12" y1="22.08" x2="12" y2="12"></line>
@@ -102,7 +102,7 @@ export const NavbarComponent = () => {
           {
             account
               ?
-              <div className="flex items-center px-4 -mx-2 -mr-20">
+              <div className="flex items-center px-4 -mx-2">
                   <h4 className="mx-2 font-medium text-white dark:text-gray-200 hover:underline">{ account.substring(0, 5) + '...' +  account.substring(account.length - 4)}</h4>
                   <img className="object-cover mx-2 rounded-full h-9 w-9" src="https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80" alt="avatar"/>
               </div>
